a75b99d1d2b7d7a10f75b154213ebe25b97519a8
bee4rt <info.visuel@gmail.com>
2023-11-18 20:00:36 +0200

    count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  //let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handelAdd() {
+    setCount(count + 1);
+  }
+  function handelSub() {
+    setCount(count - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <p>{count}</p>
+      <button onClick={handelAdd}>ADD</button>
+      <button onClick={handelSub}>SUB</button>
     </div>
   );
 }
